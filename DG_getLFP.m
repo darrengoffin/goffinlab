cb073890888fbc1d07e87a44b8f32eb4e23191a9
darrengoffin <darren.goffin@me.com>
2017-07-20 14:10:20 +0100

    Update DG_getLFP.m

--- DG_getLFP.m
+++ DG_getLFP.m
@@ -1,29 +1,48 @@
-% Recordings are made wide?band (0.1 – 6000 Hz; 20 kHz sampling rate)
-% For LFP analysis, recordings are subsequently filtered (800 Hz low?pass filter) and down?sampled (to 1250 Hz) digitally
-% LFP data and lfp sample rate are then saved into a struct named LFP
+% This function takes raw CSC data and extracts LFP
+% Recordings are made wide‐band (0.1 – 6000 Hz; 20 kHz sampling rate)
+% For LFP analysis, recordings are subsequently filtered (800 Hz low‐pass filter) and down‐sampled (to 1250 Hz) digitally
+% LFP data, lfp sample rate, and time stamps are then saved into a struct named LFP
 
 function [LFP] = DG_getLFP(baseName, data, time, sampleRate, lfpSampleRate)
 
-    % OUTPUT
-    % Creates file: basePath/baseName.lfp
+% INPUTS
+% data - contains a matrix of CSC data. Rows must represent individual electrodes. Columns must represent individual time points
+% time - time stamps for data
+% sampleRate – acquisition rate. Default = 20,000 Hz.
+% lfpSampleRate - downSampledRate. Default = 1250 Hz.
+    
+% OUTPUTS
+% Creates file: baseName.lfp.mat
 
-    % Data properties
-    numberOfElectrodes = size(data, 1);
+% LFP = DG_getLFP(baseName, data, time, sampleRate, lfpSampleRate);
+% LFP = DG_getLFP(baseName, data, time, sampleRate); Assumes lfpSampleRate = default
+% LFP = DG_getLFP(baseName, data, time); Assumes lfpSampleRate = default and sampleRate = default
+    
+if nargin < 5
+    lfpSampleRate = 1250;
+end
 
-    % Down-sampling factor 
-    factor = fix(sampleRate / lfpSampleRate);
+if nargin < 4
+    sampleRate = 20000;
+end
+
+% Data properties
+numberOfElectrodes = size(data, 1);
+
+% Down-sampling factor 
+factor = fix(sampleRate / lfpSampleRate);
     
-    % Perform filtering and downsampling
-    for electrodeIndex = 1 : numberOfElectrodes
-        buffer = FilterLFP(data(electrodeIndex, :), sampleRate, 0, 800);
-        LFP.data(electrodeIndex,:) = decimate(buffer, factor);
-    end
+% Perform filtering and downsampling
+for electrodeIndex = 1 : numberOfElectrodes
+    buffer = FilterLFP(data(electrodeIndex, :), sampleRate, 0, 800);
+    LFP.data(electrodeIndex,:) = decimate(buffer, factor);
+end
     
-    LFP.timeSamples = decimate(time, factor);
-    LFP.sampleRate = lfpSampleRate;
+LFP.timeSamples = decimate(time, factor);
+LFP.sampleRate = lfpSampleRate;
     
-    save([baseName '.lfp.mat'], 'LFP')
+save([baseName '.lfp.mat'], 'LFP')
     
-    disp('LFP file created!')
+disp('LFP file created!')
 
 end
